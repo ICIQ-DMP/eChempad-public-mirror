e605207132b01630934f0202cafd4ca89c33d0f2
AleixMT <aleixaretra@gmail.com>
2023-06-30 14:51:45 +0200

    ADDED: extracted common code of navbar, completely functional and present in profile and home page

---
+++ src/main/java/org/ICIQ/eChempad/web/ui/composers/NavbarComposer.java
@@ -0,0 +1,25 @@
+package org.ICIQ.eChempad.web.ui.composers;
+
+import org.zkoss.zk.ui.event.Event;
+import org.zkoss.zk.ui.event.EventQueue;
+import org.zkoss.zk.ui.select.SelectorComposer;
+import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zul.*;
+
+public class NavbarComposer extends SelectorComposer<Window> {
+
+
+    // UI COMPOSER METHODS
+
+    /**
+     * De-facto constructor for composer components.
+     *
+     * @param comp Window component
+     * @throws Exception If something goes wrong during initialization.
+     */
+    @Override
+    public void doAfterCompose(Window comp) throws Exception {
+        super.doAfterCompose(comp);
+
+    }
+}
