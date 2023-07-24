2977c113cb4e20d925b79ca8c424412f03103f0b
AleixMT <aleixaretra@gmail.com>
2023-07-24 21:17:52 +0200

    ADDED: license header to all files

--- src/main/java/org/ICIQ/eChempad/controllers/genericJPAControllers/AuthorityJPAControllerImpl.java
+++ src/main/java/org/ICIQ/eChempad/controllers/genericJPAControllers/AuthorityJPAControllerImpl.java
@@ -1,3 +1,23 @@
+/*
+ * eChempad is a suite of web services oriented to manage the entire
+ * data life-cycle of experiments and assays from Experimental
+ * Chemistry and related Science disciplines.
+ *
+ * Copyright (C) 2021 - present Institut Català d'Investigació Química (ICIQ)
+ *
+ * eChempad is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
 package org.ICIQ.eChempad.controllers.genericJPAControllers;
 
 import org.ICIQ.eChempad.entities.genericJPAEntities.Authority;
--- tools/Signals-API-Scratcher/scratch.sh
+++ tools/Signals-API-Scratcher/scratch.sh
@@ -1,4 +1,25 @@
 #!/usr/bin/env bash
+#
+# eChempad is a suite of web services oriented to manage the entire
+# data life-cycle of experiments and assays from Experimental
+# Chemistry and related Science disciplines.
+#
+# Copyright (C) 2021 - present Institut Català d'Investigació Química (ICIQ)
+#
+# eChempad is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+#
+
 ########################################################################################################################
 # -Name: scratch.sh
 # -Description: Scratches all the data from a certain user of Signals Notebooks by using its own API key.
