cb719cf999b31670f4629c17b40cb6110015cd79
AleixMT <aleixaretra@gmail.com>
2023-08-16 03:14:55 +0200

    ADDED: Class for controll load class weaving. apllying stack overflow anser. not working

---
+++ src/main/java/org/ICIQ/eChempad/configurations/TimeWeavingConfiguration.java
@@ -0,0 +1,11 @@
+package org.ICIQ.eChempad.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableLoadTimeWeaving;
+
+@Configuration
+@EnableLoadTimeWeaving(aspectjWeaving = EnableLoadTimeWeaving.AspectJWeaving.ENABLED)
+public class TimeWeavingConfiguration {
+
+
+}
