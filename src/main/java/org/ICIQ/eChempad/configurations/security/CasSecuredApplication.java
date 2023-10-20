5acbcd2cf5b92ff9d5d0ab4a8889a6b6dcff90f7
AleixMT <aleixaretra@gmail.com>
2023-10-20 15:26:14 +0200

    ADDED: cas support in eChempad with baeldung greedy solution

---
+++ src/main/java/org/ICIQ/eChempad/configurations/security/CasSecuredApplication.java
@@ -0,0 +1,59 @@
+package org.ICIQ.eChempad.configurations.security;
+
+import org.jasig.cas.client.validation.Cas30ServiceTicketValidator;
+import org.jasig.cas.client.validation.TicketValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.cas.ServiceProperties;
+import org.springframework.security.cas.authentication.CasAuthenticationProvider;
+import org.springframework.security.cas.web.CasAuthenticationFilter;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.logging.Logger;
+
+public class CasSecuredApplication {
+
+    //@Autowired
+    //UserDetailsServiceImpl userDetailsService;
+
+    @Bean
+    public CasAuthenticationFilter casAuthenticationFilter(
+            AuthenticationManager authenticationManager,
+            ServiceProperties serviceProperties) throws Exception {
+        CasAuthenticationFilter filter = new CasAuthenticationFilter();
+        filter.setAuthenticationManager(authenticationManager);
+        filter.setServiceProperties(serviceProperties);
+        return filter;
+    }
+
+    @Bean
+    public ServiceProperties serviceProperties() {
+        Logger.getGlobal().info("service properties");
+        ServiceProperties serviceProperties = new ServiceProperties();
+        serviceProperties.setService("http://localhost:8900/login/cas");
+        serviceProperties.setSendRenew(false);
+        return serviceProperties;
+    }
+
+    @Bean
+    public TicketValidator ticketValidator() {
+        return new Cas30ServiceTicketValidator("https://localhost:8443/cas");
+    }
+
+    @Bean
+    public CasAuthenticationProvider casAuthenticationProvider(
+            TicketValidator ticketValidator,
+            ServiceProperties serviceProperties) {
+        CasAuthenticationProvider provider = new CasAuthenticationProvider();
+        provider.setServiceProperties(serviceProperties);
+        provider.setTicketValidator(ticketValidator);
+        provider.setUserDetailsService(
+                s -> new User("casuser", "Mellon", true, true, true, true,
+                        AuthorityUtils.createAuthorityList("ROLE_ADMIN")));
+        provider.setKey("CAS_PROVIDER_LOCALHOST_8900");
+        return provider;
+    }
+
+}
