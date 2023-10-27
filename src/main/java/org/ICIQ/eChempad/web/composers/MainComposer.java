d8dae3b1525d8463ef08ccdb30f62dc601afecf5
AleixMT <aleixaretra@gmail.com>
2023-10-27 17:53:46 +0200

    REMOVED: more references to login deleted, also deleted some old comments and declarations from applicatioin.properties, most of them commented out

--- src/main/java/org/ICIQ/eChempad/web/composers/MainComposer.java
+++ src/main/java/org/ICIQ/eChempad/web/composers/MainComposer.java
@@ -66,23 +66,6 @@ public class MainComposer extends SelectorComposer<Window> {
 	public void doAfterCompose(Window comp) throws Exception {
 		// First thing to do is execute the same method in the parent
 		super.doAfterCompose(comp);
-
-		// If not authenticated redirect to login page
-		if (! this.isUserAuthenticated())
-		{
-			Executions.sendRedirect("/login");
-		}
-	}
-
-	/**
-	 * Returns true if a user is authenticated and false if the authentication is anonymous.
-	 *
-	 * @return Boolean depending on the state of authentication.
-	 */
-	public boolean isUserAuthenticated()
-	{
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		return !(authentication instanceof AnonymousAuthenticationToken);
 	}
 }
 
