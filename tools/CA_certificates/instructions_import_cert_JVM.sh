cdfb0792880b5faed5d4c1edbd2e2449d931470b
AleixMT <aleixaretra@gmail.com>
2024-02-06 17:35:03 +0100

    UPDATED: certificate update tool

--- tools/CA_certificates/instructions_import_cert_JVM.sh
+++ tools/CA_certificates/instructions_import_cert_JVM.sh
@@ -29,10 +29,14 @@
 # Probably you will not need it anymore, but if you see this error again, it has to be related with the trustStore being
 # empty or not found.
 
+#/bin/bash $HOME/Escritorio/eChempad/tools/CA_certificates/instructions_import_cert_JVM.sh  $HOME/Escritorio/eChempad/src/main/resources/security/eChempad.crt eChempad
+
+
 if [ $# -ne 2 ]; then
   echo "*** - ERROR: This scripts needs 2 args, the path to a der or pem certificate, and the alias that will begiven to this certificate. Also JAVA_HOME needs to be defined as a global variable."
   exit 2
 fi
+
 # https://connect2id.com/blog/importing-ca-root-cert-into-jvm-trust-store
 # Optional to convert from PEM to DER format
 if [ -n "$(echo "$1" | grep -Eo ".pem$" )" ]; then 
@@ -41,6 +45,7 @@ if [ -n "$(echo "$1" | grep -Eo ".pem$" )" ]; then
 else
   derfile="$1"
 fi
+
 # Ensure that the content of the file is valid and can be parsed
 "${JAVA_HOME}/bin/keytool" -v -printcert -file "${derfile}" &>/dev/null
 if [ $? -ne 0 ]; then 
@@ -49,16 +54,23 @@ if [ $? -ne 0 ]; then
 else
   echo "*** - INFO: The certificate is OK"
 fi
+
+# Delete the certificate if present
+if "${JAVA_HOME}/bin/keytool" -keystore "${JAVA_HOME}/lib/security/cacerts" -storepass "changeit" -list -alias "$2" &>/dev/null; then
+  echo "*** - INFO: certificate $2 already exists, deleting"
+  "${JAVA_HOME}/bin/keytool" -delete -alias "$2" -keystore "${JAVA_HOME}/lib/security/cacerts" -storepass "changeit"
+fi
+
 # Import the root certificate into the JVM trust store. Default pass of the CA store is "changeit"
-echo "*** - INFO: Importing certificate ${derfile} with alias $2 to ${JAVA_HOME}/jre/lib/security/cacerts using ${JAVA_HOME}/bin/keytool"
-"${JAVA_HOME}/bin/keytool" -importcert -alias "$2" -keystore "${JAVA_HOME}/jre/lib/security/cacerts" -storepass "changeit" -file "${derfile}"
-if [ $# -ne 0 ]; then
+echo "*** - INFO: Importing certificate ${derfile} with alias $2 to ${JAVA_HOME}/lib/security/cacerts using ${JAVA_HOME}/bin/keytool"
+"${JAVA_HOME}/bin/keytool" -importcert -noprompt -alias "$2" -keystore "${JAVA_HOME}/lib/security/cacerts" -storepass "changeit" -file "${derfile}" &>/dev/null
+if [ $? -ne 0 ]; then
   echo "*** - INFO: Something went wrong"
   exit 0
 fi
 
-# Verify that the root certificate has been imported searching using the alias. 
-if [ -z $("${JAVA_HOME}/bin/keytool" -keystore "${JAVA_HOME}/jre/lib/security/cacerts" -storepass "changeit" -list | grep -o "$2") ]; then
+# Verify that the root certificate has been imported searching using the alias.
+if ! "${JAVA_HOME}/bin/keytool" -keystore "${JAVA_HOME}/lib/security/cacerts" -storepass "changeit" -list -alias "$2" &>/dev/null; then
   echo "*** - WARNING: The certificate has not been detected in the store after the addition."
 else
   echo "*** - INFO: the certificate has been installed correctly"
