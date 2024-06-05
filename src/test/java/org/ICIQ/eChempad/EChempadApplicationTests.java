0d9755c21c34a66ef22245ba7780501f95c49684
AleixMT <aleixaretra@gmail.com>
2024-06-05 16:22:23 +0200

    REMOVED: test classes

--- src/test/java/org/ICIQ/eChempad/EChempadApplicationTests.java
+++ src/test/java/org/ICIQ/eChempad/EChempadApplicationTests.java
@@ -20,10 +20,6 @@
  */
 package org.ICIQ.eChempad;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 public class EChempadApplicationTests {
 
 }
